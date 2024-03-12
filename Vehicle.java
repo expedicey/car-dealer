a36836300976e0f54938367701d25ed01faa8754
expedicey <158525666+expedicey@users.noreply.github.com>
2024-03-12 10:13:49 +0700

    Add files via upload

--- Vehicle.java
+++ Vehicle.java
@@ -13,5 +13,7 @@ public abstract class Vehicle {
         this.price = price;
     }
 
-    public abstract void displayVehicleDetails();
+    public abstract void displayVehicleDetails(); {
+        
+    }
 }
